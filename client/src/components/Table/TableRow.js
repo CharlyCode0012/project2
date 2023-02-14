1fbd275fb6380d399ff3a53e638a2ab51724e946
CharlyCode0012 <100393106+CharlyCode0012@users.noreply.github.com>
2023-02-14 00:39:07 -0600

    Prototipo de interfaz de dudas y ajuste al componente Table y TableRow

--- client/src/components/Table/TableRow.js
+++ client/src/components/Table/TableRow.js
@@ -2,36 +2,66 @@ import React, { useContext } from "react";
 import { BsFillPencilFill, BsFillXSquareFill } from "react-icons/bs";
 import CrudContext from "../../Context/CrudContext";
 
-
-export default function TableRow({ el, keys, index, admin }) {
- 
+export default function TableRow({ el, keys, index, admin, buttons }) {
   let { id } = el;
   const { findData, deleteData } = useContext(CrudContext);
   let style = keys.length <= 3 ? "w-25 me-5 text-center" : "text-center";
+
   return (
     <tr>
       <td>{index + 1}</td>
       {keys.map((data, index) => (
-        <td className={style} key={el.id + index}>{el[data].toString()}</td>
+        <td className={style} key={id + index}>
+          {el[data].toString()}
+        </td>
       ))}
       {admin && (
         <td className={style}>
-          <button
-            className="btn btn-primary m-1"
-            onClick={() => {
-              findData(id);
-            }}
-          >
-            <BsFillPencilFill />
-          </button>
-          <button
-            className="btn btn-danger pb-2"
-            onClick={() => deleteData(el.name, id)}
-          >
-            <BsFillXSquareFill />
-          </button>
+          <RenderButtons
+            findData={findData}
+            deleteData={deleteData}
+            el={el}
+            buttons={buttons}
+          />
         </td>
       )}
     </tr>
   );
 }
+
+const Buttons = ({ findData, deleteData, el }) => {
+  let { id, name } = el;
+  return (
+    <>
+      <button
+        className="btn btn-primary m-1"
+        onClick={() => {
+          findData(id);
+        }}
+      >
+        <BsFillPencilFill />
+      </button>
+      <button
+        className="btn btn-danger pb-2"
+        onClick={() => deleteData(name, id)}
+      >
+        <BsFillXSquareFill />
+      </button>
+    </>
+  );
+};
+
+const Answers = () => {
+  return (
+    <>
+      <p>Hola</p>
+    </>
+  );
+};
+
+const RenderButtons = ({ buttons, findData, deleteData, el }) => {
+  console.log("buttons", buttons);
+  if (buttons)
+    return <Buttons findData={findData} deleteData={deleteData} el={el} />;
+  else return <Answers />;
+};
--- client/src/pages/Productos/Duda.js
+++ client/src/pages/Productos/Duda.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import CrudDuda from '../../components/Productos/Dudas/CrudDuda.js';
+import { CrudProvider } from "../../Context/CrudContext";
+const keys = ["name", "description","state"];
+const th = ["Nombre", "Descripcion" ,"Estado"]; 
+//const url = "https://server-production-4487.up.railway.app/api/catalogs; //prod
+const url = "http://localhost:3200/api/catalogs"; //dev-back
+//const url = "http://localhost:5000/usuarios"; //dev-false-back
 
 const Duda = () => {
   return (
-    <div>
-        <h2>Dudas</h2>
-    </div>
-  )
-}
+    <>
+      <CrudProvider url={url} path="/producto/catalogo">
+        <CrudDuda th={th} keys={keys} buttons={false}/>
+      </CrudProvider>
+    </>
+  );
+};
 
 export default Duda
