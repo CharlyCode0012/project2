c97e18d6a85a97560fc2fd11e3634855130f9b0b
CharlyCode0012 <100393106+CharlyCode0012@users.noreply.github.com>
2023-03-21 11:28:53 -0600

    ThemeProvider y Rutas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,28 @@
 import React from "react";
-import {
-	BrowserRouter as Router,
-	Routes as Switch,
-	Route,
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 
-import Login from "./pages/Login/Login";
-import Home from "./pages/Home/Home";
-import Categorias from "pages/Productos/Categories/Categorias";
-import Productos from "pages/Productos/Productos";
 import Rutas from "@/Rutas";
+
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+	palette: {
+		mode: "dark",
+	},
+});
 
 const App: React.FC = () => {
 	const avoidError = 0;
 
 	return (
-		<Router>
-			<Rutas />
-		</Router>
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<Router>
+				<Rutas />
+			</Router>
+		</ThemeProvider>
 	);
 };
 
---
+++ client/src/pages/Error404.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error404 = () => {
+	const num = 0;
+	return (
+		<div>
+			<h2>Error404: NOT FOUND</h2>
+		</div>
+	);
+};
+
+export default Error404;
