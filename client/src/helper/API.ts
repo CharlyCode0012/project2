76db561d2d1a95b12cba2a4e8b45800ffbb8f9a8
CharlyCode0012 <charly.code0012@gmail.com>
2023-03-24 12:54:30 -0600

    axios_post

--- client/src/helper/API.ts
+++ client/src/helper/API.ts
@@ -1,20 +1,33 @@
 import axios from "axios";
-import { ParamsHttp } from "models/ParamsHttp";
 
-const customAxios = () => {
-	const conn = axios;
+axios.defaults.baseURL = "https://api.example.com";
+
+axios.defaults.headers.post["Content-Type"] =
+	"application/x-www-form-urlencoded";
+
+const get = async <T>(endpoint: string, options: any) => {
+	try {
+		const response = await axios.get<T>(endpoint, options);
+		const info = await response.data;
+		return info;
+	}
+	catch (error: any) {
+		throw new Error("hubo un error", error.message);
+	}
 };
 
-const get = async <T>(Url: string): Promise<T> => {
+const post = async <T>(endpoint: string, options: any): Promise<T> => {
 	try {
-		const response = await axios.get<T>(Url);
-		return response.data;
+		const response = axios.post(endpoint, options);
+		const info = await (await response).data;
+		return info;
 	}
-	catch (error) {
-		throw new Error();
+	catch (error: any) {
+		throw new Error("hubo un error", error.message);
 	}
 };
 
 export const http = {
 	get,
+	post,
 };
