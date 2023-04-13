4564e5d9ee2ae8e8572e08aaee16032070370090
Alejandro David García González <88684972+TheMagicDragonMX@users.noreply.github.com>
2023-04-13 13:45:44 -0600

    Type for displayed delivery places

--- client/src/models/PlacesDelivery.ts
+++ client/src/models/PlacesDelivery.ts
@@ -6,3 +6,13 @@ export interface PlacesDelivery {
 	open_h: string;
 	close_h: string;
 }
+
+export interface DisplayedDeliveryPlace {
+	id: number;
+	township: string;
+	street: string;
+	colony: string;
+	homeNumber: number;
+	cp: number;
+	schedule: string;
+}
