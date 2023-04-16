69c74ff675a930e774bcd193b4f1f31a89073355
Alejandro David García González <88684972+TheMagicDragonMX@users.noreply.github.com>
2023-04-16 12:02:17 -0600

    User and UserForm components

--- client/src/pages/Usuarios/Usuarios.tsx
+++ client/src/pages/Usuarios/Usuarios.tsx
@@ -1,11 +1,268 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Navbar from "@/Navbar/Navbar";
+import {
+	Box,
+	Container,
+	IconButton,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	TableSortLabel
+} from "@mui/material";
+import { AddCircle, DeleteForever, Edit } from "@mui/icons-material";
+import Modal from "@/Modal/Modal";
+import { useSnackbar } from "notistack";
+import { instance } from "helper/API";
+import { QueryOrder, SearchAppBar } from "@/Navbar/SearchAppBar";
+import { User } from "models/User";
+import UserForm from "./UsuariosForm";
 
 const Usuarios = () => {
-	const num = 0;
+	/**
+	 * Displays notifications to the user
+	 */
+	const { enqueueSnackbar } = useSnackbar();
+
+	/**
+	 * Headers that will be displayed to the table
+	 */
+	const tableHeaders = ["ID", "Nombre", "Fecha de nacimiento", "Tipo", "Correo", "Contraseña", "Celular"];
+
+	/**
+	 * Options that the user can select to filter the data
+	 * that is displayed in the table
+	 */
+	const searchOptions = ["ID", "Nombre"];
+
+	/**
+	 * Saves the users stored in the DB
+	 * and displays them in the table
+	 */
+	const [users, setUsers] = useState<User[]>([]);
+
+	/**
+	 * Determines if the users's modal will be shown
+	 * (only when user creates or edits a user)
+	 */
+	const [showFormModal, setShowModal] = useState(false);
+	const openFormModal = () => setShowModal(true);
+	const closeFormModal = () => setShowModal(false);
+
+	/**
+	 * Keeps track of the user that the user wanted to
+	 * edit (stays null when user creates a new user)
+	 */
+	const selectedUserToEdit = useRef<User | boolean>(false);
+
+	/**
+	 * When rendered, users are obtained from DB and displayed in the table
+	 */
+	useEffect(() => {
+		fetchUsers();
+	}, []);
+
+	/**
+	 * Fetches users from the DB
+	 * Shows a notification to user when something went wrong
+	 */
+	async function fetchUsers () {
+		try {
+			const { data: users } = await instance.get<User[]>("/users");
+			setUsers(users);
+		}
+
+		catch {
+			enqueueSnackbar("Hubo un error al mostrar los usuarios", { variant: "error" });
+		}
+	}
+
+	/**
+	 * Gets called when a user was created or edited, closes the modal,
+	 * notifies the user and refreshes the table
+	 * 
+	 * @param wasAnUpdate changes the notification message depending on the action (created / edited) 
+	 */
+	function onUserSubmitted (wasAnUpdate: boolean) {
+		closeFormModal();
+		enqueueSnackbar(wasAnUpdate ? "Se actualizó con éxito" : "Se creo con éxito", { variant: "success" });
+		fetchUsers();
+	}
+
+	/**
+	 * When called, establishes that there is nothing to edit 
+	 * (as a new one is being created) and opens the modal 
+	 */
+	function createUser () {
+		selectedUserToEdit.current = false;
+		openFormModal();
+	}
+
+	/**
+	 * When called, establishes that there is a user edit 
+	 * keeps it's reference and opens the modal so it can access
+	 * the info of the about-to-edit user
+	 */
+	function editUser (user: User) {
+		selectedUserToEdit.current = user;
+		openFormModal();
+	}
+
+	/**
+	 * Calls the server to delete the given user
+	 * 
+	 * @param user data from the user that will be deleted
+	 */
+	async function deleteUser (deletedUser: User) {
+		try {
+			await instance.delete(`/users/${deletedUser.id}`);
+			enqueueSnackbar("Usuario eliminado con exito", { variant: "success" });
+			setUsers((users) => users.filter((user) => user.id !== deletedUser.id));
+		}
+
+		catch {
+			enqueueSnackbar("No se pudo eliminar", { variant: "error" });
+		}
+	}
+
+	/**
+	 * Retrieves specific users from the DB, depending on the 
+	 * filter and search of the user
+	 * 
+	 * @param filter what field will be used to filter
+	 * @param search what the user is searching
+	 * @param order either ASC or DESC
+	 */
+	async function onSubmitSearch(filter: string, search: string, order: QueryOrder) {
+		try {
+			let users: User[];
+
+			switch (filter) {
+			case "ID":
+				users = (await instance.get<User[]>("/users/searchByID", { params: { order, search } })).data;
+				break;
+				
+			case "Nombre":
+				users = (await instance.get<User[]>("/users/searchByName", { params: { order, search } })).data;
+				break;
+				
+			default:
+				users = (await instance.get<User[]>("/users", { params: { order } })).data;
+			}
+			
+			setUsers(users);
+		}
+
+		catch {
+			enqueueSnackbar("Hubo un error al mostrar los usuarios", { variant: "error" });
+		}
+	}
+
 	return (
 		<>
 			<Navbar />
+
+			<Container maxWidth="sm">
+				<Box
+					sx={{
+						height: "560px",
+						flexGrow: 1,
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center",
+					}}
+				>
+					<Box
+						sx={{
+							display: "flex",
+							flexDirection: "column",
+							gap: "10px",
+						}}
+					>
+						<SearchAppBar searchOptions={searchOptions} onSubmitSearch={onSubmitSearch} />
+						
+						<h1>Usuarios</h1>
+						
+						{showFormModal && (
+							<Modal
+								open={showFormModal}
+								handleOpen={setShowModal}
+								title={(selectedUserToEdit.current instanceof Object) ? "Editar un usuario" : "Añadir un usuario"}
+							>
+								<UserForm
+									onSubmit={onUserSubmitted}
+									userData={(selectedUserToEdit.current instanceof Object) ? selectedUserToEdit.current : undefined}
+								></UserForm>
+							</Modal>
+						)}
+						
+						<TableContainer
+							sx={{ width: "1000px", maxHeight: "400px" }}
+							component={Paper}
+							elevation={5}
+						>
+							<Table>
+								<TableHead>
+									<TableRow>
+										{tableHeaders.map((header) => (
+											<TableCell key={header} align="left">
+												<TableSortLabel>{header}</TableSortLabel>
+											</TableCell>
+										))}
+									</TableRow>
+								</TableHead>
+
+								<TableBody>
+									{!(users?.length > 0) ? (
+										<TableRow>
+											<TableCell>Sin datos</TableCell>
+										</TableRow>
+									) : (
+										users?.map((user) => (
+											<TableRow key={user.id}>
+												<TableCell align="left">{user.id}</TableCell>
+												<TableCell align="left">{user.name}</TableCell>
+												<TableCell align="left">{user.date_B.toString()}</TableCell>
+												<TableCell align="left">{user.type_use[0].toUpperCase() + user.type_use.slice(1)}</TableCell>
+												<TableCell align="left">{user.e_mail}</TableCell>
+												<TableCell align="left">{user.pass}</TableCell>
+												<TableCell align="left">{user.cel}</TableCell>
+
+												<TableCell align="center">
+													{
+														user.type_use !== "vendedor"
+															? <>
+																<IconButton onClick={() => editUser(user)}>
+																	<Edit fontSize="inherit" />
+																</IconButton>
+																<IconButton
+																	onClick={() => deleteUser(user)}
+																>
+																	<DeleteForever fontSize="inherit" />
+																</IconButton>
+															</>
+
+															: <></>
+													}
+												</TableCell>
+											</TableRow>
+										))
+									)}
+								</TableBody>
+							</Table>
+						</TableContainer>
+						<IconButton
+							sx={{ alignSelf: "flex-start", fontSize: "40px", padding: "0px" }}
+							onClick={() => createUser()}
+						>
+							<AddCircle fontSize="inherit" />
+						</IconButton>
+					</Box>
+				</Box>
+			</Container>
 		</>
 	);
 };
---
+++ client/src/pages/Usuarios/UsuariosForm.tsx
@@ -0,0 +1,209 @@
+import React, { useState } from "react";
+import {
+	Box,
+	Button,
+	FormControl,
+	IconButton,
+	InputAdornment,
+	InputLabel,
+	MenuItem,
+	OutlinedInput,
+	Select,
+	TextField,
+} from "@mui/material";
+import { useSnackbar } from "notistack";
+import { instance } from "helper/API";
+import { User } from "models/User";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+
+interface UserFormProps {
+	userData?: User;
+	onSubmit: (wasAnUpdate: boolean) => void;
+}
+
+const UserForm: React.FC<UserFormProps> = ({
+	userData,
+	onSubmit,
+}) => {
+	/**
+	 * Displays notifications to the user
+	 */
+	const { enqueueSnackbar } = useSnackbar();
+
+	/**
+	 * Determines if the password will be shown or hidden
+	 * in its input field
+	 */
+	const [showPassword, setShowPassword] = useState(false);
+
+	/**
+	 * Takes what was filled in the form and saves a new
+	 * user in the DB
+	 * 
+	 * @param event Form event that contains all of its info
+	 */
+	async function createUser (event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+
+		// Get user data from the form
+		const data = new FormData(event.currentTarget);
+		const name = data.get("name")?.toString();
+		const birthday = data.get("date_B")?.toString();
+		const userType = data.get("type_use")?.toString().toLowerCase();
+		const email = data.get("e_mail")?.toString();
+		const password = data.get("pass")?.toString();
+		const cellphone = data.get("cel")?.toString();
+
+		// Upload data to DB
+		try {
+			await instance.post("/users", {
+				id: Date.now().toString(),
+				name,
+				date_B: new Date(birthday ?? "2000-01-01"),
+				type_use: userType,
+				e_mail: email,
+				pass: password,
+				cel: cellphone
+			} as User);
+
+			onSubmit(false); // "false" tells the submission wasn't an update, it was a new user creation
+		}
+
+		catch {
+			enqueueSnackbar("Algo salio mal", { variant: "error" });
+		}
+	}
+
+	/**
+	 * Takes what was filled in the form and updates the data
+	 * from the given user in the DB
+	 * 
+	 * @param event Form event that contains all of its info
+	 */
+	async function updateUser (event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
+
+		// Get user data from the form
+		const data = new FormData(event.currentTarget);
+		const name = data.get("name")?.toString();
+		const birthday = data.get("date_B")?.toString();
+		const userType = data.get("type_use")?.toString();
+		const email = data.get("e_mail")?.toString();
+		const password = data.get("pass")?.toString();
+		const cellphone = data.get("cel")?.toString();
+
+		// Update data to DB
+		try {
+			await instance.put(`/users/${userData?.id}`, {
+				name,
+				date_B: new Date(birthday ?? "2000-01-01"),
+				type_use: userType,
+				e_mail: email,
+				pass: password,
+				cel: cellphone
+			} as User);
+
+			onSubmit(true); // "true" tells the submission was an update
+		}
+
+		catch {
+			enqueueSnackbar("Algo salio mal", { variant: "error" });
+		}
+	}
+
+	return (
+		<Box
+			component="form"
+			sx={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center",
+				gap: "1rem",
+				padding: 2,
+			}}
+			onSubmit={userData ? updateUser : createUser}
+		>
+			<TextField
+				sx={{ width: "300px" }}
+				label="Nombre"
+				name="name"
+				variant="outlined"
+				defaultValue={userData?.name}
+				type="text"
+				required
+			/>
+
+			<TextField
+				sx={{ width: "300px" }}
+				label="Fecha de nacimiento"
+				name="date_B"
+				variant="outlined"
+				defaultValue={userData?.date_B}
+				type="date"
+				required
+			/>
+
+			<FormControl>
+				<InputLabel>Filtrar por</InputLabel>
+
+				<Select
+					defaultValue={"Ayudante"}
+					label="Tipo de usuario"
+					name="type_use"
+					sx={{ width: "300px" }}
+				>
+					<MenuItem value={"Admin"}>{"Admin"}</MenuItem>
+					<MenuItem value={"Ayudante"}>{"Ayudante"}</MenuItem>
+				</Select>
+			</FormControl>
+
+			<TextField
+				sx={{ width: "300px" }}
+				label="Correo"
+				name="e_mail"
+				variant="outlined"
+				defaultValue={userData?.e_mail}
+				type="email"
+				required
+			/>
+
+			<FormControl sx={{ width: "300px" }}>
+				<InputLabel htmlFor="password">Contraseña</InputLabel>
+				<OutlinedInput
+					id="pass"
+					label="Contraseña"
+					name="pass"
+					type={showPassword ? "text" : "password"}
+					defaultValue={userData?.pass}
+					endAdornment={
+						<InputAdornment position="end">
+							<IconButton
+								edge="end"
+								onClick={() => setShowPassword(!showPassword)}
+							>
+								{showPassword ? <Visibility /> : <VisibilityOff />}
+							</IconButton>
+						</InputAdornment>
+					}
+				/>
+			</FormControl>
+
+			<TextField
+				sx={{ width: "300px" }}
+				label="Celular"
+				name="cell"
+				variant="outlined"
+				defaultValue={userData?.pass}
+				type="text"
+				inputProps={{ pattern: "^\\d{2}-\\d{4}-\\d{4}$", maxLength: 12, inputMode: "numeric" }}
+				required
+			/>
+
+			<Button type="submit" variant="contained" fullWidth>
+				{ userData ? "Actualizar" : "Crear" }
+			</Button>
+		</Box>
+	);
+};
+
+export default UserForm;
