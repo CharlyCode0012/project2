07c9f67521b240604079ec5efc9b226e061aeb10
Alejandro David García González <88684972+TheMagicDragonMX@users.noreply.github.com>
2023-04-23 12:48:31 -0600

    Displayed menu

--- client/src/models/Menu.ts
+++ client/src/models/Menu.ts
@@ -1,7 +1,30 @@
-export interface MenuData extends Object {
+export interface MenuData {
+	id: string;
+	title: string;
+	instruction: string;
+	options: MenuOption[];
+}
+
+export type OptionActionType = "menu" | "catalog" | "link" | "message";
+
+// TODO: Update type for messages and links
+export type OptionAction = MenuData | Catalog | string
+
+export interface MenuOption {
+	id: string;
 	index: number;
 	brief: string;
 	description: string;
 	keywords: string[];
-	action: null;
+	actionType: OptionActionType;
+	action: OptionAction;
 }
+
+export interface Catalog {
+	id: string;
+	name: string;
+	description: string;
+	products: string[];
+}
+
+// TODO: Create models for message and action
