c6a3711ee98a0f01f861c82f59f31506b2d4ec85
CharlyCode0012 <100393106+CharlyCode0012@users.noreply.github.com>
2023-05-16 13:34:03 -0600

    Update products.js

--- Bot-Charlyv2/base-baileys-mysql/request/products.js
+++ Bot-Charlyv2/base-baileys-mysql/request/products.js
@@ -6,5 +6,6 @@ const instance = require('instance');
 async function fetchProducts(){
     const {data: products } = await instance.get('/products');
 
-    const data = products.map(product => ({body: [`*${product.name}:* ${product}]}))
+    const data = products.map( product => ( {body: [ `*${ product.name }:* ${ product } `] }));
 }
+
