f0537d1e9185333cefafe0a85261bc63264aa6f5
CharlyCode0012 <100393106+CharlyCode0012@users.noreply.github.com>
2023-08-02 23:04:10 -0600

    delete old version from bot

--- ChatBot/base-baileys-mysql/src/flows/adapterProvider.js
+++
@@ -1,6 +0,0 @@
-const BaileysProvider = require("@bot-whatsapp/provider/baileys");
-const { createProvider } = require("@bot-whatsapp/bot");
-
-const adapterProvider = createProvider(BaileysProvider);
-
-module.exports = { adapterProvider };
